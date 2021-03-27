be5aa661e6ae5c5e9fbf968a00c50227973f4dc0
wen9xin <64579831+wen9xin@users.noreply.github.com>
2021-03-27 21:26:06 +0100

    1.2 tout fini

--- 1_2.v
+++ 1_2.v
@@ -32,5 +32,26 @@ Compute testCor.
 
 
 Definition cnat:= (T->T)->(T->T).
-Definition c0: cnat :=fun f x =>x .
+Definition c0:cnat :=fun f x =>x .
 Definition c1:cnat:=fun f x=>f x.
+Definition c2:cnat:=fun f x => f(f x).
+Definition c3:cnat:=fun f x => f(f(f x)).
+Definition c4:cnat:=fun f x => f(f(f(f x))).
+
+Definition csucc:cnat->cnat := fun n => fun f x => f(n f x).
+Definition TestCsucc:= csucc c1.
+Compute  TestCsucc. (* 1++ ==2*)
+
+Definition cplus:cnat->cnat->cnat := fun n m => fun f x => n f (m f x).
+Definition TestCplus:= cplus c2 c3.
+Compute  TestCplus. (* 2+3==5*)
+
+Definition cmul:cnat->cnat->cnat := fun n m=> fun f => n (m f).
+Definition TestCmul := cmul c2 c3.
+Compute  TestCmul. (*2*3==6*)
+
+Definition ceq0:cnat->cbool :=fun n => fun x y => n( fun z=> y) x.
+Definition TestCeq0tr := ceq0 c0.
+Compute  TestCeq0tr.  (* 0==0 -> ctr*)
+Definition TestCeq0fa := ceq0 c1.
+Compute  TestCeq0fa. (* 1!=0 -> cfa*)
